2983066721df264aebebb58b3fb467802c31c078
supattra pangjun <supattra.pa@kkumail.com>
2024-09-06 01:06:51 +0700

    d

--- app/Http/Controllers/RequestsController.php
+++ app/Http/Controllers/RequestsController.php
@@ -11,18 +11,14 @@ class RequestsController extends Controller
     public function showCourseTas()
     {
         $requests = Requests::with(['courseTas.course.subjects', 'courseTas.student'])->get();
-
-        // Return the view with the requests data
         return view('layouts.ta.statusRequest', compact('requests'));
 
-    //  /**
-    //  * Show the application dashboard.
-    //  *
-    //  * @return \Illuminate\Contracts\Support\Renderable
-    //  */
-    // public function statusRequest()
-    // {
-    //     return view('layouts.ta.statusRequest');
-    // }
+    }
+
+    public function showCourseTasAdmin()
+    {
+        $requests = Requests::with(['courseTas.course.subjects', 'courseTas.student'])->get();
+        return view('layouts.adminHome', compact('requests'));
+
     }
 }
